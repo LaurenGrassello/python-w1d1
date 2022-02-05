a53aaa909741bacbc350832ce67353cb58765547
Lauren Odalen <97255159+LaurenOdalen@users.noreply.github.com>
2022-02-05 12:34:30 -0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+int_to_float = float(35)
+float_to_int = int(44.2)
+int_to_complex = complex(35)
+print(int_to_float)
+print(float_to_int)
+print(int_to_complex)
+print(type(int_to_float))
+print(type(float_to_int))
+print(type(int_to_complex))
+
+
+
+# test two
+
+import random
+print(random.randint(2,5)) 
